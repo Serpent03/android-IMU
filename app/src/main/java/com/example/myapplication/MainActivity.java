92c9dce5f4d53b512597e8d8a04cbc3a2cc95089
Serpent03 <dumbpilot03@gmail.com>
2023-05-28 19:09:27 +0530

    feat: addition of new logic

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,26 +2,71 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.TextView;
 
-import javax.security.auth.callback.Callback;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity {
 
     MobileIMUData mIMU;
+    TextView tv;
+    Timer textUpdateTimer = new Timer();
+    int delay = 0;
+    int period = 100;
+    // call the text update function every 100ms
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mIMU = new MobileIMUData(this);
+
+        tv = (TextView) findViewById(R.id.IMUVAL);
+        // mIMU = new MobileIMUData(this);
     }
 
     public void buttonClickFunc(View view) {
         mIMU.close();
         Log.i("DESTROYING", "Window is destroyed");
+    }
+
+    public void buttonClickFuncStart(View view) {
+        Log.i("CREATING", "Window is created");
+        try {
+            textUpdateTimer.scheduleAtFixedRate(new TimerTask()
+            {
+                public void run()
+                {
+                    updateSensorValText();
+                }
+            }, delay, period);
+            mIMU = new MobileIMUData(this);
+        } catch (Exception e) {
+            Log.i("ERR_START", String.valueOf(e));
+        }
+    }
+
+    @SuppressLint("DefaultLocale")
+        public void updateSensorValText() {
+        try {
+            String st = String.format(
+                    "\nX: %f\nY: %f\nZ: %f\nts: %d",
+                    mIMU.accValues.get(0),
+                    mIMU.accValues.get(1),
+                    mIMU.accValues.get(2),
+                    mIMU.startTime
+            );
+            Log.i("SENS_VAL", st);
+            tv.setText(st);
+        } catch (Exception e) {
+            Log.i("ERR", String.valueOf(e));
+            // set the frequency to 1000ms if we haven't started
+        }
+
     }
 
 //    Callback cb = new Callback() {
--- app/src/main/java/com/example/myapplication/MobileIMUData.java
+++ app/src/main/java/com/example/myapplication/MobileIMUData.java
@@ -12,51 +12,65 @@ import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class MobileIMUData implements SensorEventListener {
     android.hardware.SensorManager mSensorManager;
-    long startTime=0;
-    Context context ;
+    public long startTime = 0;
+    Context context;
     private Callback callback;
+    public ArrayList<Double> accValues = new ArrayList<Double>();
     ArrayList<String> velocityValues;
-    String TAG="MobileIMU";
+    String TAG = "MobileIMU";
     Sensor accelerometer;
+
     public MobileIMUData(Activity activity) {
         this.callback = callback;
-        context= activity.getApplicationContext();
-        velocityValues= new ArrayList<>();
-        velocityValues.add("a_x,a_y,a_z,TimeMillisecond \n" );
-        mSensorManager = (android.hardware.SensorManager)context.getSystemService(Context.SENSOR_SERVICE);
-        // accelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_LINEAR_ACCELERATION);
+        accValues.add(0.0d);
+        accValues.add(0.0d);
+        accValues.add(0.0d);
+        context = activity.getApplicationContext();
+        velocityValues = new ArrayList<>();
+        velocityValues.add("a_x,a_y,a_z,TimeMillisecond \n");
+        mSensorManager = (android.hardware.SensorManager) context.getSystemService(Context.SENSOR_SERVICE);
         accelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_LINEAR_ACCELERATION);
-        Log.i(TAG,"Constructur called");
-        mSensorManager.registerListener(this, accelerometer, android.hardware.SensorManager.SENSOR_DELAY_NORMAL);
+//        accelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        Log.i(TAG, "Constructur called");
+        mSensorManager.registerListener(this, accelerometer, android.hardware.SensorManager.SENSOR_DELAY_FASTEST);
     }
+
     @Override
     public void onSensorChanged(SensorEvent sensorEvent) {
-      Log.i(TAG,""+sensorEvent.values[0] +" "+sensorEvent.sensor.getType());
+//        Log.i(TAG, "" + sensorEvent.values[0] + " " + sensorEvent.sensor.getType());
+        accValues.set(0, roundToPrecision(sensorEvent.values[0], 4));
+        accValues.set(1, roundToPrecision(sensorEvent.values[1], 4));
+        accValues.set(2, roundToPrecision(sensorEvent.values[2], 4));
         startTime = System.currentTimeMillis();
         String data = "";
         for (int x = 0; x < sensorEvent.values.length; x++) {
             data += sensorEvent.values[x] + ",";
         }
-        data += startTime+"\n";
-      velocityValues.add(data);
+        data += startTime + "\n";
+        velocityValues.add(data);
+    }
+
+    public double roundToPrecision(double num, int precision) {
+        return (double) Math.round(num * Math.pow(10d, precision)) / Math.pow(10d, precision);
     }
 
     @Override
     public void onAccuracyChanged(Sensor sensor, int i) {
     }
-    public  void wirteCSV(ArrayList<String> name,String fileName){
+
+    public void wirteCSV(ArrayList<String> name, String fileName) {
         try {
 
             File file = null;
             if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.R) {
-                file = new File(context.getExternalFilesDir(null).getAbsolutePath() +fileName);
-                Log.i(TAG," path"+ file.getAbsolutePath());
-            }
-            else {
-                file = new File(context.getExternalFilesDir(null).getAbsolutePath() +fileName);
+                file = new File(context.getExternalFilesDir(null).getAbsolutePath() + fileName);
+                Log.i(TAG, " path" + file.getAbsolutePath());
+            } else {
+                file = new File(context.getExternalFilesDir(null).getAbsolutePath() + fileName);
             }
             // if file doesnt exists, then create it
             if (!file.exists()) {
@@ -65,7 +79,7 @@ public class MobileIMUData implements SensorEventListener {
 
             FileWriter fw = new FileWriter(file.getAbsoluteFile());
             BufferedWriter bw = new BufferedWriter(fw);
-            for (int x=0;x<name.size();x++) {
+            for (int x = 0; x < name.size(); x++) {
                 bw.write(name.get(x));
             }
             bw.close();
@@ -75,13 +89,13 @@ public class MobileIMUData implements SensorEventListener {
         }
 
     }
-    public boolean close(){
-        wirteCSV(velocityValues,"wakingWithMovile.csv");
+
+    public boolean close() {
+        // wirteCSV(velocityValues, "wakingWithMovile.csv");
         try {
             mSensorManager.unregisterListener(this);
             return true;
-        }
-        catch (Exception e){
+        } catch (Exception e) {
             return false;
         }
     }
