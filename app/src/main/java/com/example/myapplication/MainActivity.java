5fc87a9f256eadced1c5bbd809b42b19ae2040a2
Serpent03 <dumbpilot03@gmail.com>
2023-05-29 14:53:17 +0530

    feat: smoothening filters

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -3,10 +3,16 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
+
+import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.Viewport;
+import com.jjoe64.graphview.series.DataPoint;
+import com.jjoe64.graphview.series.LineGraphSeries;
 
 import java.util.Timer;
 import java.util.TimerTask;
@@ -14,10 +20,17 @@ import java.util.TimerTask;
 public class MainActivity extends AppCompatActivity {
 
     MobileIMUData mIMU;
-    TextView tv;
+    TextView tvA;
+    TextView tvV;
+    TextView tvP;
     Timer textUpdateTimer = new Timer();
     int delay = 0;
-    int period = 100;
+    int period = 50;
+    boolean isRunning = false;
+
+    GraphView gv_AMAG;
+    LineGraphSeries<DataPoint> magAcc;
+    LineGraphSeries<DataPoint> magAccSmooth;
     // call the text update function every 100ms
 
     @Override
@@ -25,17 +38,44 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        tv = (TextView) findViewById(R.id.IMUVAL);
+        tvA = findViewById(R.id.IMUVALA);
+        tvV = findViewById(R.id.IMUVALV);
+        tvP = findViewById(R.id.IMUVALP);
+
+        gv_AMAG = findViewById(R.id.A_MAG);
+
+        Viewport vp = gv_AMAG.getViewport();
+        vp.setXAxisBoundsManual(true);
+        vp.setMinX(0);
+        vp.setMaxX(1000);
+
+        vp.setYAxisBoundsManual(true);
+        vp.setMinY(-3);
+        vp.setMaxY(3);
+
+        gv_AMAG.setTitle("A_mag");
+
+        magAcc = new LineGraphSeries<>();
+        magAccSmooth = new LineGraphSeries<>();
+        magAcc.setColor(Color.RED);
+        magAccSmooth.setColor(Color.GREEN);
+
+        gv_AMAG.addSeries(magAcc);
+        gv_AMAG.addSeries(magAccSmooth);
+
         // mIMU = new MobileIMUData(this);
     }
 
     public void buttonClickFunc(View view) {
         mIMU.close();
         Log.i("DESTROYING", "Window is destroyed");
+        magAcc = new LineGraphSeries<>();
+        isRunning = false;
     }
 
     public void buttonClickFuncStart(View view) {
         Log.i("CREATING", "Window is created");
+        isRunning = true;
         try {
             textUpdateTimer.scheduleAtFixedRate(new TimerTask()
             {
@@ -52,22 +92,47 @@ public class MainActivity extends AppCompatActivity {
 
     @SuppressLint("DefaultLocale")
         public void updateSensorValText() {
+        if (!isRunning) { return; }
         try {
-            String st = String.format(
-                    "\nX: %f\nY: %f\nZ: %f\nts: %d",
+            String stA = String.format(
+                    "Xa: %f\nYa: %f\nZa: %f\nDt: %f",
                     mIMU.accValues.get(0),
                     mIMU.accValues.get(1),
                     mIMU.accValues.get(2),
-                    mIMU.startTime
+                    mIMU.smoothMagAcc
             );
-            Log.i("SENS_VAL", st);
-            tv.setText(st);
+            String stV = String.format(
+                    "Xv: %f\nYv: %f\nZv: %f",
+                    mIMU.velValues.get(0),
+                    mIMU.velValues.get(1),
+                    mIMU.velValues.get(2)
+            );
+            String stP = String.format(
+                    "Xp: %f\nYp: %f\nZp: %f",
+                    mIMU.posValues.get(0) * 100,
+                    mIMU.posValues.get(1) * 100,
+                    mIMU.posValues.get(2) * 100
+            );
+
+            Log.i("SENS_VAL", stA);
+            tvA.setText(stA);
+            tvV.setText(stV);
+            tvP.setText(stP);
+            updGraph();
+
         } catch (Exception e) {
             Log.i("ERR", String.valueOf(e));
             // set the frequency to 1000ms if we haven't started
         }
 
     }
+
+    public void updGraph() {
+        magAcc.appendData(new DataPoint(mIMU.timeAlive, mIMU.magAcc.get(mIMU.magAcc.size() - 1)), true, 400);
+        magAccSmooth.appendData(new DataPoint(mIMU.timeAlive, mIMU.smoothMagAcc), true, 400);
+    }
+
+
 
 //    Callback cb = new Callback() {
 //        @Override
--- app/src/main/java/com/example/myapplication/MobileIMUData.java
+++ app/src/main/java/com/example/myapplication/MobileIMUData.java
@@ -5,22 +5,36 @@ import android.content.Context;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.util.Log;
 
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.lang.reflect.Array;
 import java.util.ArrayList;
-import java.util.Arrays;
 
 public class MobileIMUData implements SensorEventListener {
     android.hardware.SensorManager mSensorManager;
-    public long startTime = 0;
+    double curTime = System.currentTimeMillis();
+    double oldTime = curTime;
+    double startTime = curTime;
+
+    double timeAlive = curTime - startTime;
+
+    public double dT = curTime - oldTime;
     Context context;
     private Callback callback;
     public ArrayList<Double> accValues = new ArrayList<Double>();
-    ArrayList<String> velocityValues;
+    public ArrayList<Double> velValues = new ArrayList<Double>();
+    public ArrayList<Double> posValues = new ArrayList<Double>();
+
+    public double smoothMagAcc = 0.0d;
+
+    double minFilterValue = 9999;
+    ArrayList<Double> magAcc = new ArrayList<Double>();
+
     String TAG = "MobileIMU";
     Sensor accelerometer;
 
@@ -29,29 +43,81 @@ public class MobileIMUData implements SensorEventListener {
         accValues.add(0.0d);
         accValues.add(0.0d);
         accValues.add(0.0d);
+        velValues.add(0.0d);
+        velValues.add(0.0d);
+        velValues.add(0.0d);
+        posValues.add(0.0d);
+        posValues.add(0.0d);
+        posValues.add(0.0d);
+
+        magAcc.add(0.0d);
+        magAcc.add(0.0d);
+        magAcc.add(0.0d);
+
         context = activity.getApplicationContext();
-        velocityValues = new ArrayList<>();
-        velocityValues.add("a_x,a_y,a_z,TimeMillisecond \n");
         mSensorManager = (android.hardware.SensorManager) context.getSystemService(Context.SENSOR_SERVICE);
         accelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_LINEAR_ACCELERATION);
 //        accelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
         Log.i(TAG, "Constructur called");
-        mSensorManager.registerListener(this, accelerometer, android.hardware.SensorManager.SENSOR_DELAY_FASTEST);
+        mSensorManager.registerListener(this, accelerometer, SensorManager.SENSOR_DELAY_FASTEST);
     }
 
     @Override
     public void onSensorChanged(SensorEvent sensorEvent) {
 //        Log.i(TAG, "" + sensorEvent.values[0] + " " + sensorEvent.sensor.getType());
-        accValues.set(0, roundToPrecision(sensorEvent.values[0], 4));
-        accValues.set(1, roundToPrecision(sensorEvent.values[1], 4));
-        accValues.set(2, roundToPrecision(sensorEvent.values[2], 4));
-        startTime = System.currentTimeMillis();
-        String data = "";
-        for (int x = 0; x < sensorEvent.values.length; x++) {
-            data += sensorEvent.values[x] + ",";
+        curTime = System.currentTimeMillis();
+        timeAlive = curTime - startTime;
+
+        accValues.set(0, roundToPrecision(sensorEvent.values[0], 2));
+        accValues.set(1, roundToPrecision(sensorEvent.values[1], 2));
+        accValues.set(2, roundToPrecision(sensorEvent.values[2], 2));
+
+        double term1 = magAcc.get(magAcc.size() - 1);
+        double term2 = magAcc.get(magAcc.size() - 2);
+        double term3 = magAcc.get(magAcc.size() - 3);
+
+        magAcc.add(magnitudeOf(accValues));
+        smoothMagAcc = rollingAverage(magAcc, 20);
+        minFilterValue = Math.min((term1 + term2 + term3) / 3, minFilterValue);
+        Log.i("MAG", "" + (minFilterValue < term1 - minFilterValue));
+
+        dT = (curTime - oldTime) / 1000.0;
+
+        if (magAcc.get(magAcc.size() -1) < minFilterValue) {
+            accValues.set(0, 0.0d);
+            accValues.set(1, 0.0d);
+            accValues.set(2, 0.0d);
         }
-        data += startTime + "\n";
-        velocityValues.add(data);
+
+        velValues = integrate(velValues, accValues, dT);
+        posValues = integrate(posValues, velValues, dT);
+
+        oldTime = curTime;
+//        String data = "";
+//        for (int x = 0; x < sensorEvent.values.length; x++) {
+//            data += sensorEvent.values[x] + ",";
+//        }
+//        data += startTime + "\n";
+//        velocityValues.add(data);
+    }
+
+    public double rollingAverage(ArrayList<Double> magAcc, int window) {
+        double sum = 0.0d;
+        window = Math.min(window, magAcc.size() - 1);
+        for (int i = 1; i < window; i++) {
+            sum += magAcc.get(magAcc.size() - i);
+        }
+        return sum / window;
+    }
+    public double magnitudeOf(ArrayList<Double> pVec) {
+        return Math.sqrt(Math.pow(pVec.get(0), 2) + Math.pow(pVec.get(1), 2) + Math.pow(pVec.get(2), 2));
+    }
+
+    public ArrayList<Double> integrate(ArrayList<Double> pVal, ArrayList<Double> iVal, double dT) {
+        pVal.set(0, pVal.get(0) + (iVal.get(0) * dT));
+        pVal.set(1, pVal.get(1) + (iVal.get(1) * dT));
+        pVal.set(2, pVal.get(2) + (iVal.get(2) * dT));
+        return pVal;
     }
 
     public double roundToPrecision(double num, int precision) {
